84e4717a3f3228d8064c8f5d99c67ba399d9f86e
Walisson Silva <walissonsilva.dev@gmail.com>
2023-12-04 18:36:50 -0300

    feat: change colorscheme to catppuccin theme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  "catppuccin/nvim",
+  lazy = false,
+  name = "catppuccin",
+  priority = 1000,
+  opts = function()
+    return {
+      transparent_background = true,
+    }
+  end,
+}
